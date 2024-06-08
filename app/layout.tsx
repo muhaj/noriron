3eaa042c1fcf261d5877ceaf609070684e99d4ea
Jetnor Muhaj <jmuhaj@gmail.com>
2024-06-08 16:20:51 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "Noriron",
+  description: "Noriron is a modern Web3 banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
