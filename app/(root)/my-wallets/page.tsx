c8937a162cb7e9500d968623ee9a3052fcafc96a
Jetnor Muhaj <jmuhaj@gmail.com>
2024-06-09 13:26:59 +0100

    update

--- app/(root)/my-wallets/page.tsx
+++ app/(root)/my-wallets/page.tsx
@@ -1,114 +1,52 @@
+// app/(root)/my-wallets/page.tsx
+"use client";
+
 import React, { useState } from "react";
 import sdk from "@crossmarkio/sdk";
 
-function App() {
+const MyWallets = () => {
   const [signInResponse, setSignInResponse] = useState("");
   const [sessionUserResponse, setSessionUserResponse] = useState("");
   const [signTransactionTxblob, setSignTransactionTxblob] = useState("");
-  const [submitTransactionResponse, setSubmitTransactionResponse] =
-    useState("");
+  const [submitTransactionResponse, setSubmitTransactionResponse] = useState("");
 
   const signIn = async () => {
-    // Sign in logic here
     let address = (await sdk.async.signInAndWait()).response.data.address;
-    return setSignInResponse(address);
+    setSignInResponse(address);
   };
 
   const getUserSession = async () => {
-    // Session logic here
     let id = sdk.session.user?.id;
-    if (id) return setSessionUserResponse(id);
+    if (id) setSessionUserResponse(id);
   };
 
   const signTransaction = async () => {
-    // Sign transaction logic here
     let resp = await sdk.async.signAndWait({
       TransactionType: "Payment",
       Account: signInResponse,
       Destination: "rK8jihXBZCFWZqX95SET3CCi1WdRgZQwex", // Add any destination address here
       Amount: "11000000", // XRP in drops
     });
-    if (resp) return setSignTransactionTxblob(resp.response.data.txBlob);
+    if (resp) setSignTransactionTxblob(resp.response.data.txBlob);
   };
 
   const submitTransaction = async () => {
-    // Submit transaction logic here
-    let resp = await sdk.async.submitAndWait(
-      signInResponse,
-      signTransactionTxblob
-    );
-    if (resp)
-      return setSubmitTransactionResponse(resp.response.data.resp.result.hash);
+    let resp = await sdk.async.submitAndWait(signInResponse, signTransactionTxblob);
+    if (resp) setSubmitTransactionResponse(resp.response.data.resp.result.hash);
   };
 
   return (
     <div>
-      <img
-        alt="Crossmark logo"
-        className="logo"
-        src="./titleblock.png"
-        style={{ position: "absolute", top: 0, left: 0, height: 50 }}
-      />
-      <div className="wrapper">
-        <h1>Starter Template</h1>
-      </div>
-
-      <main className="main">
-        <div>
-          <div className="content">
-            <button onClick={signIn} className="rounded-button">
-              Sign In and Wait
-            </button>
-            <div style={{ paddingLeft: "10rem", paddingTop: "5rem" }}></div>
-            <textarea
-              style={{ width: "75%", height: "10rem", color: "black" }}
-              id="response1"
-              readOnly
-              value={`Address: ${signInResponse}`}
-            ></textarea>
-          </div>
-          <div className="divider"></div>
-          <div className="content">
-            <button onClick={getUserSession} className="rounded-button">
-              Get Session
-            </button>
-            <div style={{ paddingLeft: "11rem", paddingTop: "5rem" }}></div>
-            <textarea
-              style={{ width: "75%", height: "10rem", color: "black" }}
-              id="response2"
-              readOnly
-              value={`Current User ID: ${sessionUserResponse}`}
-            ></textarea>
-          </div>
-          <div className="divider"></div>
-          <div className="content">
-            <button onClick={signTransaction} className="rounded-button">
-              Sign Transaction
-            </button>
-            <div style={{ paddingLeft: "10rem", paddingTop: "5rem" }}></div>
-            <textarea
-              style={{ width: "75%", height: "10rem", color: "black" }}
-              id="response3"
-              value={`TxBlob: ${signTransactionTxblob}`}
-            ></textarea>
-          </div>
-          <div className="divider"></div>
-          <div className="content">
-            <button onClick={submitTransaction} className="rounded-button">
-              Submit Transaction
-            </button>
-            <div style={{ paddingLeft: "10rem", paddingTop: "5rem" }}></div>
-            <textarea
-              style={{ width: "75%", height: "10rem", color: "black" }}
-              id="response4"
-              value={`Hash: ${submitTransactionResponse}`}
-            ></textarea>
-          </div>
-          <div className="divider"></div>
-        </div>
-      </main>
+      <button onClick={signIn}>Sign In</button>
+      <textarea readOnly value={`Address: ${signInResponse}`}></textarea>
+      <button onClick={getUserSession}>Get Session</button>
+      <textarea readOnly value={`Current User ID: ${sessionUserResponse}`}></textarea>
+      <button onClick={signTransaction}>Sign Transaction</button>
+      <textarea readOnly value={`TxBlob: ${signTransactionTxblob}`}></textarea>
+      <button onClick={submitTransaction}>Submit Transaction</button>
+      <textarea readOnly value={`Hash: ${submitTransactionResponse}`}></textarea>
     </div>
   );
-}
+};
 
-export default App;
+export default MyWallets;
