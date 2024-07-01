955c25335fadcaf759d4ea23cd78ee04148011cd
Jetnor Muhaj <jmuhaj@gmail.com>
2024-07-01 22:24:26 +0100

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,40 +7,10 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Footer from './Footer';
 import PlaidLink from './PlaidLink';
-import { useState } from 'react';
-import sdk from "@crossmarkio/sdk";
+
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
-  const [signInResponse, setSignInResponse] = useState("");
-  const [sessionUserResponse, setSessionUserResponse] = useState("");
-  const [signTransactionTxblob, setSignTransactionTxblob] = useState("");
-  const [submitTransactionResponse, setSubmitTransactionResponse] = useState("");
-
-  const signIn = async () => {
-    let address = (await sdk.async.signInAndWait()).response.data.address;
-    setSignInResponse(address);
-  };
-
-  const getUserSession = async () => {
-    let id = sdk.session.user?.id;
-    if (id) setSessionUserResponse(id);
-  };
-
-  const signTransaction = async () => {
-    let resp = await sdk.async.signAndWait({
-      TransactionType: "Payment",
-      Account: signInResponse,
-      Destination: "rK8jihXBZCFWZqX95SET3CCi1WdRgZQwex", // Add any destination address here
-      Amount: "11000000", // XRP in drops
-    });
-    if (resp) setSignTransactionTxblob(resp.response.data.txBlob);
-  };
-
-  const submitTransaction = async () => {
-    let resp = await sdk.async.submitAndWait(signInResponse, signTransactionTxblob);
-    if (resp) setSubmitTransactionResponse(resp.response.data.resp.result.hash);
-  };
 
   return (
     <section className="sidebar">
@@ -50,14 +20,14 @@ const Sidebar = ({ user }: SiderbarProps) => {
             src="/icons/logo.svg"
             width={34}
             height={34}
-            alt="Noriron logo"
+            alt="Horizon logo"
             className="size-[24px] max-xl:size-14"
           />
-          <h1 className="sidebar-logo">Noriron</h1>
+          <h1 className="sidebar-logo">Horizon</h1>
         </Link>
 
         {sidebarLinks.map((item) => {
-          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
 
           return (
             <Link href={item.route} key={item.label}
@@ -77,49 +47,15 @@ const Sidebar = ({ user }: SiderbarProps) => {
                 {item.label}
               </p>
             </Link>
-          );
+          )
         })}
-
-        <button onClick={signIn} className="sidebar-link">
-          <div className="relative size-6">
-            <Image 
-              src="/icons/wallet.svg"
-              alt="My Wallets"
-              fill
-            />
-          </div>
-          <p className="sidebar-label">My Wallets</p>
-        </button>
-
+        
         <PlaidLink user={user} />
       </nav>
 
-      <div className="mt-4">
-        <textarea
-          className="w-full h-24 bg-gray-700 text-white p-2"
-          readOnly
-          value={`Address: ${signInResponse}`}
-        ></textarea>
-        <textarea
-          className="w-full h-24 bg-gray-700 text-white p-2 mt-2"
-          readOnly
-          value={`Current User ID: ${sessionUserResponse}`}
-        ></textarea>
-        <textarea
-          className="w-full h-24 bg-gray-700 text-white p-2 mt-2"
-          readOnly
-          value={`TxBlob: ${signTransactionTxblob}`}
-        ></textarea>
-        <textarea
-          className="w-full h-24 bg-gray-700 text-white p-2 mt-2"
-          readOnly
-          value={`Hash: ${submitTransactionResponse}`}
-        ></textarea>
-      </div>
-
       <Footer user={user} />
     </section>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
